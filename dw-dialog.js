44fca5652c1c35eae1f77d9bd5bed4e5cfcffba0
Nirmal Baldaniya <nirmalb@codetailor.in>
2020-07-13 15:20:09 +0530

    fix(dw-dialog): fix transform

--- dw-dialog.js
+++ dw-dialog.js
@@ -317,20 +317,19 @@ export class DwDialog extends LitElement {
    * @param {Object} e Event
    */
   _viewportHandler(e) {
-    const tick = () => {
-      const container = this.shadowRoot.querySelector('#dialogContainer');
-      container.style.maxHeight = e.target.height + 'px';
-      const containerTop = container.getBoundingClientRect().top;
-      // This condition required due to behavior of CSS's fixed property. 
-      // For reference visit : https://developer.mozilla.org/en-US/docs/Web/CSS/position 
-      const offsetTop = e.target.offsetTop;
-      if (containerTop !== 0 && offsetTop != 0) {
-        container.style.transform = 'translateY(' + e.target.offsetTop + 'px)';
-      } else {
-        container.style.transform = 'none';
-      }
+    const container = this.shadowRoot.querySelector('#dialogContainer');
+    container.style.maxHeight = e.target.height + 'px';
+    const containerTop = container.getBoundingClientRect().top;
+    const offsetTop = e.target.offsetTop;
+    // This condition required due to behavior of CSS's fixed property. 
+    // For reference visit : https://developer.mozilla.org/en-US/docs/Web/CSS/position 
+    console.log('container top', containerTop);
+    console.log('offsetTop', offsetTop);
+    if (containerTop !== 0) {
+      container.style.transform = 'translateY(' + offsetTop + 'px)';
+    } else {
+      container.style.transform = 'none';
     }
-    window.requestAnimationFrame(tick);
   }
 
   /**
