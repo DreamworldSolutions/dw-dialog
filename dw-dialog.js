7139beff4b32be62f3bcf49bbdfde5dca6597f4a
Nirmal Baldaniya <nirmalb@codetailor.in>
2020-07-15 15:25:17 +0530

    fix(-dw-dialog): use requestAnimationFrame to transform dialog.

--- dw-dialog.js
+++ dw-dialog.js
@@ -317,20 +317,23 @@ export class DwDialog extends LitElement {
    * @param {Object} e Event
    */
   _viewportHandler(e) {
-    const container = this.shadowRoot.querySelector('#dialogContainer');
-    container.style.maxHeight = e.target.height + 'px';
-    const containerTop = container.getBoundingClientRect().top;
-    const offsetTop = e.target.offsetTop;
-    // This condition required due to behavior of CSS's fixed property. 
-    // For reference visit : https://developer.mozilla.org/en-US/docs/Web/CSS/position 
-    console.log(this.tagName, 'targetHeight', e.target.height);
-    console.log(this.tagName, 'containerTop', containerTop);
-    console.log(this.tagName, 'targetOffsetTop', offsetTop);
-    if (containerTop !== 0 && offsetTop !== 0) {
-      container.style.transform = 'translateY(' + offsetTop + 'px)';
-    } else {
-      container.style.transform = 'none';
+    const tick = () => {
+      const container = this.shadowRoot.querySelector('#dialogContainer');
+      container.style.maxHeight = e.target.height + 'px';
+      const containerTop = Math.abs(Math.trunc(container.getBoundingClientRect().top));
+      const offsetTop = e.target.offsetTop;
+      // This condition required due to behavior of CSS's fixed property. 
+      // For reference visit : https://developer.mozilla.org/en-US/docs/Web/CSS/position 
+      console.log(this.tagName, 'targetHeight', e.target.height);
+      console.log(this.tagName, 'containerTop', containerTop);
+      console.log(this.tagName, 'targetOffsetTop', offsetTop);
+      if (containerTop !== 0 && offsetTop !== 0) {
+        container.style.transform = 'translateY(' + offsetTop + 'px)';
+      } else {
+        container.style.transform = 'none';
+      }
     }
+    window.requestAnimationFrame(tick);
   }
 
   /**
