926d1895d068911e2583740e2a92ee842343a9dc
Nirmal Baldaniya <nirmalb@codetailor.in>
2021-01-13 11:46:18 +0530

    BREAKING CHANGE: update "mwc" components to 0.20.0

--- component.js
+++ component.js
@@ -37,7 +37,7 @@ var MDCDialog = /** @class */ (function (_super) {
 
     Object.defineProperty(MDCDialog.prototype, "isOpen", {
         get: function () {
-            return this.foundation_.isOpen();
+            return this.foundation.isOpen();
         },
         enumerable: true,
         configurable: true
@@ -45,10 +45,10 @@ var MDCDialog = /** @class */ (function (_super) {
 
     Object.defineProperty(MDCDialog.prototype, "escapeKeyAction", {
         get: function () {
-            return this.foundation_.getEscapeKeyAction();
+            return this.foundation.getEscapeKeyAction();
         },
         set: function (action) {
-            this.foundation_.setEscapeKeyAction(action);
+            this.foundation.setEscapeKeyAction(action);
         },
         enumerable: true,
         configurable: true
@@ -56,10 +56,10 @@ var MDCDialog = /** @class */ (function (_super) {
 
     Object.defineProperty(MDCDialog.prototype, "scrimClickAction", {
         get: function () {
-            return this.foundation_.getScrimClickAction();
+            return this.foundation.getScrimClickAction();
         },
         set: function (action) {
-            this.foundation_.setScrimClickAction(action);
+            this.foundation.setScrimClickAction(action);
         },
         enumerable: true,
         configurable: true
@@ -67,10 +67,10 @@ var MDCDialog = /** @class */ (function (_super) {
 
     Object.defineProperty(MDCDialog.prototype, "autoStackButtons", {
         get: function () {
-            return this.foundation_.getAutoStackButtons();
+            return this.foundation.getAutoStackButtons();
         },
         set: function (autoStack) {
-            this.foundation_.setAutoStackButtons(autoStack);
+            this.foundation.setAutoStackButtons(autoStack);
         },
         enumerable: true,
         configurable: true
@@ -82,14 +82,15 @@ var MDCDialog = /** @class */ (function (_super) {
 
     MDCDialog.prototype.initialize = function () {
         var e_1, _a;
-        var container = this.root_.querySelector(strings.CONTAINER_SELECTOR);
+        console.log(this.root, this.root)
+        var container = this.root.querySelector(strings.CONTAINER_SELECTOR);
         if (!container) {
             throw new Error("Dialog component requires a " + strings.CONTAINER_SELECTOR + " container element");
         }
         this.container_ = container;
-        this.content_ = this.root_.querySelector(strings.CONTENT_SELECTOR);
-        this.buttons_ = [].slice.call(this.root_.querySelectorAll(strings.BUTTON_SELECTOR));
-        this.defaultButton_ = this.root_.querySelector("[" + strings.BUTTON_DEFAULT_ATTRIBUTE + "]");
+        this.content_ = this.root.querySelector(strings.CONTENT_SELECTOR);
+        this.buttons_ = [].slice.call(this.root.querySelectorAll(strings.BUTTON_SELECTOR));
+        this.defaultButton_ = this.root.querySelector("[" + strings.BUTTON_DEFAULT_ATTRIBUTE + "]");
         this.buttonRipples_ = [];
         try {
             for (var _b = tslib_1.__values(this.buttons_), _c = _b.next(); !_c.done; _c = _b.next()) {
@@ -111,9 +112,9 @@ var MDCDialog = /** @class */ (function (_super) {
 
     MDCDialog.prototype.initialSyncWithDOM = function () {
         var _this = this;
-        this.handleClick_ = this.foundation_.handleClick.bind(this.foundation_);
-        this.handleKeydown_ = this.foundation_.handleKeydown.bind(this.foundation_);
-        this.handleDocumentKeydown_ = this.foundation_.handleDocumentKeydown.bind(this.foundation_);
+        this.handleClick_ = this.foundation.handleClick.bind(this.foundation);
+        this.handleKeydown_ = this.foundation.handleKeydown.bind(this.foundation);
+        this.handleDocumentKeydown_ = this.foundation.handleDocumentKeydown.bind(this.foundation);
         this.handleLayout_ = this.layout.bind(this);
         var LAYOUT_EVENTS = ['resize', 'orientationchange'];
         this.handleOpening_ = function () {
@@ -147,18 +148,18 @@ var MDCDialog = /** @class */ (function (_super) {
     };
 
     MDCDialog.prototype.layout = function () {
-        this.foundation_.layout();
+        this.foundation.layout();
     };
 
     MDCDialog.prototype.open = function () {
-        this.foundation_.open();
+        this.foundation.open();
     };
 
     MDCDialog.prototype.close = function (action) {
         if (action === void 0) {
             action = '';
         }
-        this.foundation_.close(action);
+        this.foundation.close(action);
     };
 
     MDCDialog.prototype.getDefaultFoundation = function () {
@@ -172,7 +173,7 @@ var MDCDialog = /** @class */ (function (_super) {
             },
 
             addClass: function (className) {
-                return _this.root_.classList.add(className);
+                return _this.root.classList.add(className);
             },
 
             areButtonsStacked: function () {
@@ -215,7 +216,7 @@ var MDCDialog = /** @class */ (function (_super) {
             },
 
             hasClass: function (className) {
-                return _this.root_.classList.contains(className);
+                return _this.root.classList.contains(className);
             },
 
             isContentScrollable: function () {
@@ -246,7 +247,7 @@ var MDCDialog = /** @class */ (function (_super) {
                 return document.body.classList.remove(className);
             },
             removeClass: function (className) {
-                return _this.root_.classList.remove(className);
+                return _this.root.classList.remove(className);
             },
 
             reverseButtons: function () {
