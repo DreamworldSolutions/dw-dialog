9733ebacfcfc9916cb5a82c9fe956bb48dfd71a5
Shakil Mansuri <shakil.mansuri@dreamworldsolutions.com>
2021-03-23 11:10:41 +0530

    feat: provide a way to change popover dialog box-shadow.

--- popover-dialog-css.js
+++ popover-dialog-css.js
@@ -19,7 +19,7 @@ export const popoverStyle = css`
   }
 
   #popover_dialog__surface {
-    box-shadow: 0px 11px 15px -7px rgba(0, 0, 0, 0.2), 0px 24px 38px 3px rgba(0, 0, 0, 0.14), 0px 9px 46px 8px rgba(0, 0, 0, 0.12);
+    box-shadow: var(--dw-popover-box-shadow, 0px 11px 15px -7px rgba(0, 0, 0, 0.2), 0px 24px 38px 3px rgba(0, 0, 0, 0.14), 0px 9px 46px 8px rgba(0, 0, 0, 0.12));
     border-radius: var(--dw-popover-border-radius, 4px);
     background: var(--mdc-theme-surface, #FFF);
     min-width: var(--dw-popover-min-width, 280px);
