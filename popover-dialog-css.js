d3994b8a58b846efa3f49aa0bf52b49560701fdd
Nirmal Baldaniya <nirmalb@codetailor.in>
2021-07-01 15:47:57 +0530

    fix: popover radius and shadow issue

--- popover-dialog-css.js
+++ popover-dialog-css.js
@@ -58,11 +58,12 @@ export const externalStyle = css`
     will-change: opacity, transform;
     animation-name: fadeIn;
     animation-duration: var(--dw-popover-animation-time, 0.3s);
-    z-index: 1;
+    z-index: 99;
   }
 
   .tippy-box {
-    box-shadow: var(--dw-popover-box-shadow, 0px 11px 15px -7px rgba(0, 0, 0, 0.2), 0px 24px 38px 3px rgba(0, 0, 0, 0.14), 0px 9px 46px 8px rgba(0, 0, 0, 0.12));
+    box-shadow: var(--dw-popover-box-shadow, var(--mdc-elevation--z2, 0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12)));
+    border-radius: 5px;
   }
 
   .tippy-box:focus {
