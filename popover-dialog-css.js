08dc30310ff85fbe0322e3f36a4be8b97e4b9746
KheniApexa <45095454+KheniApexa@users.noreply.github.com>
2021-10-23 14:14:56 +0530

    fix(dw-popover-dialog): set css variable for border radius.

--- popover-dialog-css.js
+++ popover-dialog-css.js
@@ -63,7 +63,7 @@ export const externalStyle = css`
 
   .tippy-box {
     box-shadow: var(--dw-popover-box-shadow, var(--mdc-elevation--z2, 0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12)));
-    border-radius: 5px;
+    border-radius: var(--dw-popover-border-radius, 4px);
   }
 
   .tippy-box:focus {
