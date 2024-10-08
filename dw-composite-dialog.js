dcd45b88940723efec42742eb8cab46876871296
Nirmal Baldaniya <nirmal.baldaniya@gmail.com>
2024-10-08 17:22:01 +0530

    fix: text are blurred some times.

--- dw-composite-dialog.js
+++ dw-composite-dialog.js
@@ -14,7 +14,7 @@ import { Shadow } from '@dreamworld/material-styles/shadow.js';
  * Purpose: Composes `modal`, `fit` & "popover" dialogs.
  * 
  * Usage:
- *  - This dialog can be used by extention only. e.g `<my-composite-dialog></my-composite-dialog>`
+ *  - This dialog can be used by extension only. e.g `<my-composite-dialog></my-composite-dialog>`
  */
 export class DwCompositeDialog extends DwCompositeBaseDialogMixin(DwModalDialogMixin(DwFitDialogMixin(DwPopoverDialogMixin(LitElement)))) {
   static get styles() {
@@ -24,6 +24,25 @@ export class DwCompositeDialog extends DwCompositeBaseDialogMixin(DwModalDialogM
       popoverStyle,
       fitDialogStyles
     ]
+  }
+
+  updated(props) {
+    super.updated(props);
+    if (props.has("opened") && this.opened) {
+      this._setTransformStyle();
+    }
+  }
+
+  /**
+   * This is a workaround for Safari / Chrome rendering issue at specific zoom level.
+   */
+  _setTransformStyle() {
+    setTimeout(() => {
+      const el = this.renderRoot.querySelector("#dialogContainer") || this.renderRoot.querySelector("#popover_dialog__surface");
+      if(el) {
+        el.style.transform = `translateZ(0)`;
+      }
+    }, 350);
   }
 }
 
